95c37dc085eb3e13e47005404fefd47c5a89fcd8
Marcio Alves <marcioalvesjun@gmail.com>
2026-03-23 14:21:07 -0300

    feat: created user registration and search

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,40 @@
 import express from 'express'
+import prisma from './lib/prisma'
 const app = express()
+app.use(express.json())
 const port = '3002'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  console.log('Response sent')
+app.post('/user', async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(400).json({ error: 'User already exists' })
+  }
+})
+
+app.get('/user/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+    res.status(200).json(user)
+  } catch (error) {
+    res.status(400).json({ error: 'Error' })
+  }
 })
 
 app.listen(port, () => {
